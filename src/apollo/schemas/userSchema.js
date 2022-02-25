14eb5cfd449cd36f9129fa08b283f966eb8c7db3
Netszky <chigotjulien@gmail.com>
2022-02-25 23:18:40 +0100

    cancel sub

--- src/apollo/schemas/userSchema.js
+++ src/apollo/schemas/userSchema.js
@@ -10,7 +10,7 @@ module.exports = gql `
         isSub: Boolean
         isAdmin: Boolean
         stripeID: String
-        Sub: Sub
+        sub: Sub
     }
     type Query {
         getUsers:[User]
